da35a3d6ffedc100d2d58ffa87f507e338643499
Gustavo <agvedotti@atmaconsulting.com.br>
2012-04-02 22:33:32 -0300

    segundo commit

---
+++ src/Pessoa.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;	
+	private Calendar data;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public Calendar getData() {
+		return data;
+	}
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+} 
